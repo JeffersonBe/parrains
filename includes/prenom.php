debe721df1175752606f73537e979bfb7c0b9bb2
Jefferson Bonnaire <jefferson@MacBook-Pro-de-Jefferson-Bonnaire.local>
2012-10-01 00:54:24 +0200

    Ajout de l'autocompletion et suppression des fichiers

---
+++ includes/prenom.php
@@ -0,0 +1,20 @@
+<?php
+// si on reçoit une donnée
+if(isset($_GET['q'])) {
+    $q = htmlentities($_GET['q']); // protection
+
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=parrains', 'root', 'root');
+    } catch(Exception $e) {
+        exit('Impossible de se connecter à la base de données.');
+    }
+    // écriture de la requête
+    $requete = "SELECT prenom FROM user WHERE prenom LIKE '". $q ."%' LIMIT 0, 10";
+    // exécution de la requête
+    $resultat = $bdd->query($requete) or die(print_r($bdd->errorInfo()));
+    // affichage des résultats
+    while($donnees = $resultat->fetch(PDO::FETCH_ASSOC)) {
+        echo $donnees['prenom'] ."\n";
+    }
+}
+?>
---
+++ includes/script.php
@@ -0,0 +1,7 @@
+<!-- Included JS Files (Compressed) -->
+<script src="javascripts/jquery.js"></script>
+<script src="javascripts/foundation.min.js"></script>
+
+<!-- Initialize JS Plugins -->
+<script src="javascripts/jquery.autocomplete.js"></script>
+<script src="javascripts/app.js"></script>
