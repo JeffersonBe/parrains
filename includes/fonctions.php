35f8287721be4cd9ff1c7e6bc579a03d81d87db3
Jefferson Bonnaire <jefferson@MacBook-Pro-de-Jefferson-Bonnaire.local>
2012-10-01 22:24:50 +0200

    Simplification de la fonction mail

--- includes/fonctions.php
+++ includes/fonctions.php
@@ -38,10 +38,4 @@ function mailINT($email)
 	else
 		return false;
 }
-
-function mail_utf8($to, $subject, $message, $header)
-        {
-          $header_ = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/html; charset=UTF-8' . "\r\n";
-          mail($to, '=?UTF-8?B?'.base64_encode($subject).'?=', $message, $header_ . $header);
-        }
 ?>
