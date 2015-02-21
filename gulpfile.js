6a2f6b329a8c7d206697afb366796e8fc2958c31
Jefferson BONNAIRE <jefferson.bonnaire@gmail.com>
2015-02-21 21:53:08 +0000

    Deleted default assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,39 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = gulpLoadPlugins();
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
+gulp.task("checkDev", [ "php serve artisan" ], function(callback) {
+  var options = {
+    pageUrls: [
+      'http://localhost:8000/',
+    ],
+    checkLinks: true,
+    onlySameDomain: true,
+    queryHashes: true,
+    noRedirects: true,
+    noLocalLinks: true,
+    linksToIgnore: [
+      'http://localhost:8000/404.html'
+    ],
+    checkXhtml: true,
+    checkCaching: true,
+    checkCompression: true,
+    maxResponseTime: 200,
+    userAgent: 'custom-user-agent/1.2.3',
+    summary: true
+  };
+  checkPages(console, options, callback);
+});
 
-elixir(function(mix) {
-    mix.less('app.less');
+gulp.task("checkProd", function(callback) {
+  var options = {
+    pageUrls: [
+      'http://example.com/',
+      'http://example.com/blog',
+      'http://example.com/about.html'
+    ],
+    checkLinks: true,
+    maxResponseTime: 500
+  };
+  checkPages(console, options, callback);
 });
