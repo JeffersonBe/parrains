b14c8d3989004f1ff552a81e31e3a925f6a73dd6
Jefferson BONNAIRE <jefferson.bonnaire@gmail.com>
2015-02-21 20:56:07 +0000

    Create template folder and view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
+@extends('layouts.master')
+    @section('title')
+        <title>Parrainage - Showtime BDE TMSP</title>
+    @stop
 
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-  <meta charset="utf-8" />
-
-  <!-- Set the viewport width to device width for mobile -->
-  <meta name="viewport" content="width=device-width" />
-  <title>Parrainage - Showtime BDE TMSP</title>
-</head>
-<body>
-  <div id="center" class="row">
-  <header class="twelve columns">
-    <ul class="nav-bar two-up">
-        <li class="active"><a href="index.php">Choisir son parrain</a></li>
-        <li><a href="indexcoeur.php">Choisir votre parrain de cœur</a></li>
-    </ul>
-  </header>
-  <div id="main" class="twelve columns">
-    <p class="ten columns centered lead">Le parrainage est à présent fini, nous vous remercions de votre participation.</p>
-  </div><!-- Fin de Main -->
-  <footer class="twelve columns">
-  </footer>
-  <!-- Included JS Files (Compressed) -->
-</body>
-</html>
+    @section('content')
+    @stop
